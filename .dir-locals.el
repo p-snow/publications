6bb82cdc4025a07498f97b8362eefe815cc067e2
p-snow <6841207+p-snow@users.noreply.github.com>
2023-11-30 14:36:40 +0900

    update on 2023-11-30 14:36:46

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,7 @@
-((nil . ((compile-command . "make tangle")))
+((nil . ((compile-command . "make tangle")
+         (eval . (setq output-dir
+                       (or (and (boundp 'output-dir) output-dir)
+                           (expand-file-name "./public/ライフハックの道具箱2023年版"))))))
  (org-mode . ((auto-save-visited-mode . nil)
               (eval . (add-hook 'after-save-hook
                                 #'my/compile-default-command
